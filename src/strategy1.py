382399cae811d323e7539d1a4ad8a6156575f6ff
sakcruise <sakthi.murugan.v@gmail.com>
2019-09-07 14:25:12 +0100

    Added profits stock

--- src/strategy1.py
+++ src/strategy1.py
@@ -15,17 +15,23 @@ class GetStockdata():
         self.conn = db.connect_db()
         self.output_path = sys.path[1] + "\data\output\profits\\"
 
+    def get_stock_list(self):
+        # stocklist_df = pd.read_csv(sys.path[1] + "\data\input\stock500list.csv")
+        stocklist_df = pd.read_sql("select lower(symbol) as symbol from stocks".format(), con=self.conn)
+        # print(stocklist_df)
+        stocklist =  stocklist_df['symbol'].tolist()
+        return stocklist
 
-    def get_stock_price(self):
+    def calculate_stocks_profit(self, _stocklist):
         sql = """select x2.symbol, x2.date,
                     round(cast((case when x2.buy_rate != 0 then x2.buy_rate else x2.sell_rate end) as numeric), 2) as rate,
                     case when x2.buy_rate != 0 then 'buy' else 'sell' end as rate_type
                     from
                     (select *,
                      lag(x1.buy_rate) OVER
-                    (ORDER BY x1.date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as buy_lag,
+                    (PARTITION BY X1.SYMBOL ORDER BY x1.date) as buy_lag,
                     lag(x1.sell_rate) OVER
-                    (ORDER BY x1.date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as sell_lag
+                    (PARTITION BY X1.SYMBOL ORDER BY x1.date) as sell_lag
                      from (
                     select x.symbol,
                     x.date, x.open, x.high, x.low, x.hhv, x.llv, x.rownum,
@@ -39,44 +45,50 @@ class GetStockdata():
                     over (partition by s.symbol order by s.date ROWS BETWEEN 50 PRECEDING AND 1 PRECEDING) as llv,
                     row_number() over (partition by s.symbol order by s.date) as rownum
                     from stocks_price s
-                    where s.symbol = 'abb'
-                    order by s.date) x
+                    where s.symbol = '{0}'
+                    order by s.symbol, s.date) x
                     where x.rownum >= 250) x1
                     where x1.buy_rate != 0 or x1.sell_rate !=0) x2
                     where (x2.buy_rate != 0 and (x2.buy_lag is null or x2.buy_lag = 0)) or
                      (x2.sell_rate != 0 and (x2.sell_lag is null or x2.sell_lag = 0))
                     """
-        stockprice_df = pd.read_sql(sql, con=self.conn)
-        symbol = stockprice_df['symbol'].values[0]
-        print(stockprice_df)
+        for symbol in _stocklist:
+            print(symbol)
+            stockprice_df = pd.read_sql(sql.format(symbol), con=self.conn)
+            stocks_df = pd.DataFrame(columns=['symbol', 'buy_date', 'sell_date', 'buy_rate', 'sell_rate', 'profit'])
+            i = 0
+            buy_rate = False
+            for index, row in stockprice_df.iterrows():
+                stocks_df.at[i, 'symbol'] = symbol
+                if row['rate_type'] == 'buy':
+                    stocks_df.at[i, 'buy_date'] = row['date']
+                    stocks_df.at[i, 'buy_rate'] = row['rate']
+                    buy_rate = True
 
-        stocks_df = pd.DataFrame(columns=['symbol', 'buy_date', 'sell_date', 'buy_rate', 'sell_rate', 'profit'])
-        i = 0
-        for index, row in stockprice_df.iterrows():
-            stocks_df.at[i, 'symbol'] = symbol
-            if row['rate_type'] == 'buy':
-                stocks_df.at[i, 'buy_date'] = row['date']
-                stocks_df.at[i, 'buy_rate'] = row['rate']
+                if row['rate_type'] == 'sell' and buy_rate:
+                    stocks_df.at[i, 'sell_date'] = row['date']
+                    stocks_df.at[i, 'sell_rate'] = row['rate']
+                    buy_rate = False
+                    i+=1
+            stocks_df['profit'] = stocks_df['sell_rate'] - stocks_df['buy_rate']
+            # stocks_df.to_csv(self.output_path + symbol + '_' + 'profit' + '.csv', index=False)
 
-            if row['rate_type'] == 'sell':
-                stocks_df.at[i, 'sell_date'] = row['date']
-                stocks_df.at[i, 'sell_rate'] = row['rate']
-                i+=1
-        stocks_df['profit'] = stocks_df['sell_rate'] - stocks_df['buy_rate']
+            stocks_df.to_sql(name="stocks_profit", con=self.conn, index=False, if_exists='append')
 
-        print(stocks_df.to_csv(self.output_path + symbol + '_' + 'profit' + '.csv', index=False))
-        print(stocks_df['profit'].sum())
-        # plt.scatter(date, [600.01, 112.78], marker='o')
-
-        # stockprice_df.plot(x='date', y='abb')
-        # # ax.scatter(xlist, ylist, zlist, color=color)
-        # # plt.plot(0, 0+1)
-        # plt.show()
+    def download_stocks_profit(self):
+        sql = """select extract(year from buy_date) as year,extract(month from buy_date) as month , * from stocks_profit
+                 order by buy_date"""
+        stockprice_extract_df = pd.read_sql(sql, con=self.conn)
+        stockprice_extract_df.to_csv(self.output_path + 'stocks_profit' + '.csv', index=False)
 
 
 def main():
+
     s = GetStockdata()
-    stocklist = s.get_stock_price()
+    stocklist_df = s.get_stock_list()
+    s.calculate_stocks_profit(stocklist_df)
+    s.download_stocks_profit()
+
 
 if __name__ == "__main__":
     main()
